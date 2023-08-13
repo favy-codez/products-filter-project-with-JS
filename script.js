a1212a67a166d171b54291b62893e54524f444cd
favy-codez <107569845+favy-codez@users.noreply.github.com>
2023-08-13 10:10:40 +0100

    added functionalities

--- script.js
+++ script.js
@@ -48,81 +48,78 @@ const data = [
   const categoriesContainer = document.querySelector(".cats");
   const priceRange = document.querySelector(".priceRange");
   const priceValue = document.querySelector(".priceValue");
-  
+
+//   to display content section
   const displayProducts = (filteredProducts) => {
-    productsContainer.innerHTML = filteredProducts
-      .map(
-        (product) =>
-          `
-         <div class="product">
-            <img
+    productsContainer.innerHTML = filteredProducts.map((product) => `
+    <div class="product">
+    <img
             src=${product.img}
             alt=""
             />
             <span class="name">${product.name}</span>
             <span class="priceText">$${product.price}</span>
-          </div>
-      `
-      )
-      .join("");
+    </div>
+    `
+    ).join("");
   };
-  
+
   displayProducts(data);
   
-  searchInput.addEventListener("keyup", (e) => {
-    const value = e.target.value.toLowerCase();
+//   searchInput.addEventListener("keyup", (e) => {
+//     const value = e.target.value.toLowerCase();
   
-    if (value) {
-      displayProducts(
-        data.filter((item) => item.name.toLowerCase().indexOf(value) !== -1)
-      );
-    } else {
-      displayProducts(data);
-    }
-  });
+//     if (value) {
+//       displayProducts(
+//         data.filter((item) => item.name.toLowerCase().indexOf(value) !== -1)
+//       );
+//     } else {
+//       displayProducts(data);
+//     }
+//   });
   
-  const setCategories = () => {
-    const allCats = data.map((item) => item.cat);
-    const categories = [
-      "All",
-      ...allCats.filter((item, i) => {
-        return allCats.indexOf(item) === i;
-      }),
-    ];
+//   const setCategories = () => {
+//     const allCats = data.map((item) => item.cat);
+//     const categories = [
+//       "All",
+//       ...allCats.filter((item, i) => {
+//         return allCats.indexOf(item) === i;
+//       }),
+//     ];
   
-    categoriesContainer.innerHTML = categories
-      .map(
-        (cat) =>
-          `
-        <span class="cat">${cat}</span>
-      `
-      )
-      .join("");
+//     categoriesContainer.innerHTML = categories
+//       .map(
+//         (cat) =>
+//           `
+//         <span class="cat">${cat}</span>
+//       `
+//       )
+//       .join("");
   
-    categoriesContainer.addEventListener("click", (e) => {
-      const selectedCat = e.target.textContent;
+//     categoriesContainer.addEventListener("click", (e) => {
+//       const selectedCat = e.target.textContent;
   
-      selectedCat === "All"
-        ? displayProducts(data)
-        : displayProducts(data.filter((item) => item.cat === selectedCat));
-    });
-  };
+//       selectedCat === "All"
+//         ? displayProducts(data)
+//         : displayProducts(data.filter((item) => item.cat === selectedCat));
+//     });
+//   };
   
-  const setPrices = () => {
-    const priceList = data.map((item) => item.price);
-    const minPrice = Math.min(...priceList);
-    const maxPrice = Math.max(...priceList);
+//   const setPrices = () => {
+//     const priceList = data.map((item) => item.price);
+//     const minPrice = Math.min(...priceList);
+//     const maxPrice = Math.max(...priceList);
   
-    priceRange.min = minPrice;
-    priceRange.max = maxPrice;
-    priceRange.value = maxPrice;
-    priceValue.textContent = "$" + maxPrice;
+//     priceRange.min = minPrice;
+//     priceRange.max = maxPrice;
+//     priceRange.value = maxPrice;
+//     priceValue.textContent = "$" + maxPrice;
   
-    priceRange.addEventListener("input", (e) => {
-      priceValue.textContent = "$" + e.target.value;
-      displayProducts(data.filter((item) => item.price <= e.target.value));
-    });
-  };
+//     priceRange.addEventListener("input", (e) => {
+//       priceValue.textContent = "$" + e.target.value;
+//       displayProducts(data.filter((item) => item.price <= e.target.value));
+//     });
+//   };
   
-  setCategories();
-  setPrices();
+//   setCategories();
+//   setPrices();
